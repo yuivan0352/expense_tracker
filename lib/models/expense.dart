79ebfb3a02731ef27fb45cb5edc0a5c1d005d79b
Ivan Yu <yu.ivan0352@gmail.com>
2025-05-01 12:21:00 -0400

    Chart Implemented

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -41,4 +41,20 @@ class ExpenseBucket {
 
   final Category category;
   final List<Expense> expenses;
+
+  ExpenseBucket.forCategory(
+    List<Expense> allExpenses, this.category
+  ) : expenses = allExpenses.where((expense) => 
+  expense.category == category
+  ).toList();
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -87,7 +88,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("Chart"),
+          Chart(expenses: _registeredExpenses,),
           Expanded(
             child: mainContent,
           ),
